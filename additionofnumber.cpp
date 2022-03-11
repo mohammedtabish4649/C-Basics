2304748062fb0f1c635f42982681d963c2112063
Mohammed Tabish <mohammedtabish98@gmail.com>
2022-03-12 01:57:05 +0530

    Addition of two numbers

---
+++ additionofnumber.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1,num2,ans; 
+    cout<<"Enter first number: ";
+    cin>>num1;
+    cout<<"Enter second number: ";
+    cin>>num2;
+    ans=num1+num2;
+    cout<<"addition of two numbers: "<<ans;
+
+    return 0;
+}
+
